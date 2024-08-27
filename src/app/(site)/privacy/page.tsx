574344eff60e5592afe0f7f10a0ea9c2c288a3ee
rboudrouss <r.boudrouss@gmail.com>
2024-08-27 23:24:51 +0200

    ajout politique de confidentialité

---
+++ src/app/(site)/privacy/page.tsx
@@ -0,0 +1,113 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Politique de confidentialité</h1>
+      <div>
+        <strong>Introduction</strong>
+      </div>
+      <div>
+        Devant le développement des nouveaux outils de communication, il est
+        nécessaire de porter une attention particulière à la protection de la
+        vie privée. C&apos;est pourquoi, nous nous engageons à respecter la
+        confidentialité des renseignements personnels que nous collectons.
+      </div>
+      <h2>Collecte des renseignements personnels</h2>
+      <div>
+        <ul>
+          <li>Adresse électronique</li>
+        </ul>
+      </div>
+      <div>
+        Les renseignements personnels que nous collectons sont recueillis au
+        travers de formulaires et grâce à l&apos;interactivité établie entre
+        vous et notre site Web. Nous utilisons également, comme indiqué dans la
+        section suivante, des fichiers témoins et/ou journaux pour réunir des
+        informations vous concernant.
+      </div>
+      <h2>Formulaires&nbsp;et interactivité:</h2>
+      <div>
+        Vos renseignements personnels sont collectés par le biais de formulaire,
+        à savoir :
+      </div>
+      <div>
+        <ul>
+          <li>Formulaire d&apos;inscription au site Web</li>
+        </ul>
+      </div>
+      <div>
+        Nous utilisons les renseignements ainsi collectés pour les finalités
+        suivantes :
+      </div>
+      <div>
+        <ul>
+          <li>Assurer l'unicité de la création de compte</li>
+        </ul>
+      </div>
+      <h2>Droit d&apos;opposition et de retrait</h2>
+      <div>
+        Nous nous engageons à vous offrir un droit d&apos;opposition et de
+        retrait quant à vos renseignements personnels.
+      </div>
+      <div>
+        Le droit d&apos;opposition s&apos;entend comme étant la possiblité
+        offerte aux internautes de refuser que leurs renseignements personnels
+        soient utilisées à certaines fins mentionnées lors de la collecte.
+      </div>
+      <div>
+        Le droit de retrait s&apos;entend comme étant la possiblité offerte aux
+        internautes de demander à ce que leurs renseignements personnels ne
+        figurent plus, par exemple, dans une liste de diffusion.
+      </div>
+      <div>
+        <strong>Pour pouvoir exercer ces droits, vous pouvez :</strong>
+      </div>
+      <div>Courriel :&nbsp; dev@playsorbonne.fr</div>
+      <h2>Droit d&apos;accès</h2>
+      <div>
+        Nous nous engageons à reconnaître un droit d&apos;accès et de
+        rectification aux personnes concernées désireuses de consulter,
+        modifier, voire radier les informations les concernant.
+      </div>
+      <div>
+        <strong>L&apos;exercice de ce droit se fera :</strong>
+      </div>
+      <div>Courriel :&nbsp; dev@playsorbonne.fr</div>
+      <h2>Sécurité</h2>
+      <div>
+        Les renseignements personnels que nous collectons sont conservés dans un
+        environnement sécurisé. Les personnes travaillant pour nous sont tenues
+        de respecter la confidentialité de vos informations.
+      </div>
+      <div>
+        Pour assurer la sécurité de vos renseignements personnels, nous avons
+        recours aux mesures suivantes :
+      </div>
+      <div>
+        <ul>
+          <li>Protocole SSL</li>
+          <li>Gestion des accès - personne autorisée</li>
+          <li>Gestion des accès - personne concernée</li>
+          <li>Sauvegarde informatique</li>
+          <li>Hashage des informations sensibles</li>
+        </ul>
+      </div>
+      <div>
+        Nous nous engageons à maintenir un haut degré de confidentialité en
+        intégrant les dernières innovations technologiques permettant
+        d&apos;assurer la confidentialité de vos transactions. Toutefois, comme
+        aucun mécanisme n&apos;offre une sécurité maximale, une part de risque
+        est toujours présente lorsque l&apos;on utilise Internet pour
+        transmettre des renseignements personnels.
+      </div>
+      <h2>Législation</h2>
+      <div>
+        Nous nous engageons à respecter les dispositions législatives énoncées
+        dans :
+      </div>
+      <div>
+        Législation:
+        https://www.cnil.fr/fr/reglement-europeen-protection-donnees
+      </div>
+    </>
+  );
+}
