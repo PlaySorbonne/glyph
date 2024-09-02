77a24a42d310cdc00bccdaa90a54c7891ec9118f
rboudrouss <r.boudrouss@gmail.com>
2024-09-02 14:52:43 +0200

    that wasn't it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,12 +13,14 @@ export default async function RootLayout({
   return (
     <html lang="fr">
       <head>
-        <script
-          async
-          defer
-          data-website-id="b338f2f6-7f8d-4885-b1ff-c59afe131f3f"
-          src="https://stats.backend.playsorbonne.fr/umami.js"
-        ></script>
+        {process.env.NODE_ENV !== "development" && (
+          <script
+            async
+            defer
+            data-website-id="b338f2f6-7f8d-4885-b1ff-c59afe131f3f"
+            src="https://stats.backend.playsorbonne.fr/umami.js"
+          ></script>
+        )}
       </head>
       <body className={inter.className}>
         <Suspense>
