763a6ed3699ba162b48be29b3a7a30e6a87963e2
rboudrouss <r.boudrouss@gmail.com>
2024-09-06 11:53:31 +0200

    welcome finished

--- src/app/app/welcome/3/page.tsx
+++ src/app/app/welcome/3/page.tsx
@@ -1,15 +1,27 @@
 import Link from "next/link";
-import Fraternities, { getBackground, getName, getLogo } from "@/assets/fraternities";
+import Fraternities, {
+  getBackground,
+  getName,
+  getLogo,
+} from "@/assets/fraternities";
 import Image from "next/image";
+import { cookies } from "next/headers";
+import { getUserFromSession } from "@/actions/auth";
 
 export default async function WelcomePage({
   searchParams,
 }: {
   searchParams: { fraternityId: string };
 }) {
-  const fraternityId = searchParams.fraternityId;
-  const background = getBackground(parseInt(fraternityId));
-  const fraternity = getName(parseInt(fraternityId));
+  let fraternityString =
+    searchParams.fraternityId ?? cookies().get("fraternityId")?.value;
+  const fraternityId = (
+    fraternityString
+      ? parseInt(fraternityString)
+      : await getUserFromSession().then((user) => user!.fraternityId!)
+  ) as 1 | 2 | 3;
+  const background = getBackground(fraternityId);
+  const fraternity = getName(fraternityId);
   return (
     <div
       className="min-h-screen flex flex-col justify-center items-center p-6"
@@ -19,20 +31,25 @@ export default async function WelcomePage({
         backgroundPosition: "center",
       }}
     >
-    <Image src={getLogo(parseInt(fraternityId))} alt={fraternity} width={100} height={100} />
+      <Image
+        src={getLogo(fraternityId)}
+        alt={fraternity}
+        width={100}
+        height={100}
+      />
       <div className="max-w-md w-full bg-white bg-opacity-20 backdrop-blur-lg rounded-xl shadow-lg p-8">
-        <h1 className="text-3xl font-bold mb-6 text-center" style={{
-          fontFamily: "DCC-Ash",
-          letterSpacing: "0.2rem",
-        }}>
+        <h1
+          className="text-3xl font-bold mb-6 text-center"
+          style={{
+            fontFamily: "DCC-Ash",
+            letterSpacing: "0.2rem",
+          }}
+        >
           Hmm... {fraternity} !
         </h1>
         <p className="text-lg mb-8 text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-          voluptate beatae maxime rerum quibusdam tempore temporibus molestias
-          ex, cumque possimus qui? Ex ipsa tempore nemo, modi vitae alias
-          aliquam magni iure dolor debitis odit autem rerum sit sequi, tenetur
-          quos.
+          {phrase[fraternityId] ??
+            "Vous avez été choisi pour rejoindre une fraternité légendaire. Rocher, Ciseaux ou Feuille, vous portez désormais les couleurs d'un.e des fondateur.ice de Glyph. Votre aventure commence maintenant ! "}
         </p>
         <Link
           href="/app"
@@ -44,3 +61,9 @@ export default async function WelcomePage({
     </div>
   );
 }
+
+let phrase = {
+  1: "Votre force et votre détermination inébranlables vous ancrent dans la fraternité du Rocher. Comme son fondateur Pietr, vous êtes une véritable force de la nature, prêt à surmonter tous les obstacles avec bravoure.",
+  2: "Votre esprit aiguisé et votre rapidité d'exécution vous placent sous la bannière de la fraternité des Ciseaux. Comme sa fondatrice Saka, vous coupez à travers les mystères avec finesse et intelligence.",
+  3: "Votre imagination fertile et votre connexion intuitive à l'essence-jeu vous lient à la fraternité de la Feuille. Comme san fondateur.ice Foli, vous suivez les courants du savoir avec sagesse et créativité.",
+};
