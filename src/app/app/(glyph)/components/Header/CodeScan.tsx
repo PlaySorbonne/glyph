e0996b5a20a8d6f9a7b531c76e134bbe36fbcc94
rboudrouss <r.boudrouss@gmail.com>
2024-09-11 22:28:15 +0200

    style wrong camera smh

--- src/app/app/(glyph)/components/Header/CodeScan.tsx
+++ src/app/app/(glyph)/components/Header/CodeScan.tsx
@@ -89,7 +89,6 @@ export default function CodeScan() {
                   onError={qrError}
                   onScan={qrScan}
                   style={{ width: "100%" }}
-                  facingMode="front"
                 />
               )}
             </div>
