c2d53a38b91e053e0a8b561818e2f1142a5474ac
rboudrouss <r.boudrouss@gmail.com>
2025-08-08 23:14:08 +0200

    added svg for UI rewrite

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,11 +1,17 @@
 import lock from "./lock.svg";
 import check from "./check.svg";
 import qr from "./qrcode.svg";
+import corner from "./corner.svg";
+import separator1 from "./separator1.svg";
+import separator2 from "./separator2.svg";
 
 let icons = {
   lock,
   check,
   qr,
+  corner,
+  separator1,
+  separator2,
 };
 
 export default icons;
