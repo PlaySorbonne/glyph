0fa52060558bf6081e0e2b0b42f21e2d9195b728
rboudrouss <r.boudrouss@gmail.com>
2025-08-16 00:02:15 +0200

    fix(not-found): correct "cannot udpate while rendering" since router.push updates router while still rendering NotFound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,6 +3,7 @@
 import { useFlashMessage } from "@/contexts/FlashMessageContext";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 export default function NotFound() {
   const pathname = usePathname();
@@ -15,10 +16,20 @@ export default function NotFound() {
 
   let errorMsg = "La page demandée n'existe pas.";
 
-  redirectWithError(redirectUrl, errorMsg, "Page non trouvée");
+  useEffect(() => {
+    redirectWithError(redirectUrl, errorMsg, "Page non trouvée");
+  }, []);
 
   return (
-    <p>
+    <p
+      style={{
+        fontSize: "1.5rem",
+        textAlign: "center",
+        marginTop: "5rem",
+        height: "100px",
+        width: "100%",
+      }}
+    >
       {errorMsg} Vous pouvez retourner à la{" "}
       <Link href={redirectUrl}>page d'accueil</Link>.
     </p>
